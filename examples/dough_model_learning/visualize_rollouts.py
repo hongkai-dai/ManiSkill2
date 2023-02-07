8b982394cb19d4b57361e7a1e9a0492823c2eed7
Blake Wulfe <blake.wulfe@tri.global>
2023-02-06 21:58:20 -0800

    Add code for generating rollouts and script that generates them
    The rollout code is library code with tests. Ultimately we should
    replace this with something from some existing library. I'm taking
    the approach in this commit for now in order to decouple progress
    on the model from progress on the infrastructure side.
    
    Then there are a couple scripts for generating data and
    visualizing it. These don't have tests since they're not part of
    the library code. I'll include some visualizations, though.

---
+++ examples/dough_model_learning/visualize_rollouts.py
@@ -0,0 +1,50 @@
+import os
+
+import cv2
+import fire
+import imageio
+import numpy as np
+
+from mani_skill2.utils.rollout import load_sample_batch
+
+
+def create_gif(imgs, filepath, duration=0.1):
+    assert filepath.endswith(".gif")
+    imageio.mimsave(filepath, imgs, duration=duration)
+
+
+def normalize_resize_heightmaps(heightmaps, max_height, resize):
+    normalized_heights = [(h / max_height * 255).astype(np.uint8) for h in heightmaps]
+    resized_heights = [cv2.resize(h, resize) for h in normalized_heights]
+    return resized_heights
+
+
+def create_heightmap_transition_gif(obs, next_obs, filepath, resize=(256, 256)):
+    max_height = np.max(obs)
+    obs = normalize_resize_heightmaps(obs, max_height, resize)
+    next_obs = normalize_resize_heightmaps(next_obs, max_height, resize)
+
+    transitions = []
+    for obs_i, next_obs_i in zip(obs, next_obs):
+        transition = np.hstack((obs_i, next_obs_i))
+        transitions.append(transition)
+    return create_gif(transitions, filepath, duration=0.5)
+
+
+def main(output_dir, data_filepath, max_num_episodes_to_visalize=5):
+    os.makedirs(output_dir, exist_ok=True)
+
+    batch = list(load_sample_batch(data_filepath))[0]
+    episodes = batch.split_by_episode()
+    for episode in episodes[:max_num_episodes_to_visalize]:
+        eps_id = episode["eps_id"][0]
+        filepath = os.path.join(output_dir, f"episode_{eps_id:02d}.gif")
+        create_heightmap_transition_gif(
+            episode["obs"],
+            episode["new_obs"],
+            filepath,
+        )
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
---
+++ mani_skill2/utils/rollout.py
@@ -0,0 +1,107 @@
+"""Functions relating to generating rollouts.
+
+TODO(blake.wulfe): Switch to some existing framework
+instead of rolling your own here.
+"""
+import collections
+from typing import Any, Generator
+
+import gym
+import numpy as np
+from ray.rllib.evaluation.sample_batch_builder import SampleBatchBuilder
+from ray.rllib.models.preprocessors import get_preprocessor
+from ray.rllib.offline.json_reader import JsonReader
+from ray.rllib.offline.json_writer import JsonWriter
+from ray.rllib.policy.sample_batch import SampleBatch, concat_samples
+
+# TODO(blake.wulfe): Figure out an gym agent interface.
+def rollout_episode(
+    env: gym.Env,
+    agent: Any,
+    max_steps: int,
+    episode_id: int,
+) -> SampleBatch:
+    """Rolls out an episode in env with the agent.
+
+    Args:
+        env: Environment to roll out. Gym.Env interface expected.
+        agent: Agent to generate actions.
+        max_steps: Maximum number of steps to take in env.
+        episode_id: The int id to use for this episode.
+
+    Returns:
+        SampleBatch containing the information from the episode.
+    """
+    builder = SampleBatchBuilder()
+    prep = get_preprocessor(env.observation_space)(env.observation_space)
+
+    agent.reset()
+    obs = env.reset()
+    for t in range(max_steps):
+        action = agent.step(obs)
+        new_obs, rew, done, info = env.step(action)
+
+        builder.add_values(
+            t=t,
+            eps_id=episode_id,
+            obs=prep.transform(obs),
+            actions=action,
+            rewards=rew,
+            new_obs=prep.transform(new_obs),
+            dones=done,
+            infos=info,
+        )
+
+        if done:
+            break
+        obs = new_obs
+
+    return builder.build_and_reset()
+
+
+def generate_rollouts(
+    env: gym.Env,
+    agent: Any,
+    num_episodes: int,
+    max_steps: int,
+) -> SampleBatch:
+    """Generates multiple rollouts.
+
+    Args:
+        env: Environment to roll out in.
+        agent: The agent to use for action selection.
+        num_episodes: The number of episodes to generate.
+        max_steps: Maximum number of steps per episode.
+
+    Returns:
+        A SampleBatch of the concatenated episode rollouts.
+    """
+    batches = []
+    for i in range(num_episodes):
+        batch = rollout_episode(env, agent, max_steps, i)
+        batches.append(batch)
+    return concat_samples(batches)
+
+
+def save_sample_batch(dirpath: str, batch: SampleBatch) -> None:
+    """Saves a SampleBatch to file.
+
+    Args:
+        dirpath: Directory in which to save individual files.
+        batch: The SampleBatch to save.
+    """
+    writer = JsonWriter(dirpath)
+    writer.write(batch)
+
+
+def load_sample_batch(filepath: str) -> Generator[SampleBatch, None, None]:
+    """Load a sample batch from file.
+
+    Args:
+        filepath: The filepath to load.
+
+    Returns:
+        A generator of SampleBatch objects.
+    """
+    reader = JsonReader(filepath)
+    yield from reader.read_all_files()
