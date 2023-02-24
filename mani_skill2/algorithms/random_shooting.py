1ba1c0f8db2c4a4ed0a3633a261d4c68fe753cff
Blake Wulfe <blake.wulfe@tri.global>
2023-02-23 20:38:49 -0800

    Implement evaluation of an algorithm and a trained model
    This commit glues together the code to run eval.

--- mani_skill2/algorithms/random_shooting.py
+++ mani_skill2/algorithms/random_shooting.py
@@ -1,6 +1,6 @@
-from collections.abc import Callable
-from typing import Tuple
+from typing import Callable, Tuple
 
+import numpy as np
 import torch
 
 from mani_skill2.dynamics.generative_env import GenerativeEnv
@@ -14,48 +14,59 @@ class RandomShootingAgent(GymAgent):
         action_sampler: Callable[[torch.Tensor, int], torch.Tensor],
         planning_steps: int,
         discount_factor: float = 1.0,
+        device: str = "cuda",
+        verbose_info: bool = False,
     ):
         """
         Args:
             generative_env: The environment that wraps the dynamics and the reward.
             action_sampler: action_sampler(obs, steps) returns a batch of action
-                            sequence, each sequence contains `steps` actions. Namely
-                            the returned value has shape (num_samples, steps, act_size)
+                sequence, each sequence contains `steps` actions. Namely
+                the returned value has shape (num_samples, steps, act_size)
             planning_steps: The number of steps in the planning horizon.
+            discount_factor: How much to exponentially discount future rewards.
+            device: The device on which to place tensors created in this class.
+                TODO(blake.wulfe): Once PlanningAgent is implemented, remove this.
+            verbose_info: If True, stores a large amount of information in the `info`
+                dict returned from step.
         """
         self.generative_env = generative_env
         self.action_sampler = action_sampler
         self.planning_steps = planning_steps
         self.discount_factor = discount_factor
+        self.device = device
+        self.verbose_info = verbose_info
 
     def step(
-        self, obs: torch.Tensor
+        self,
+        obs: np.ndarray,
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        """
-        Plans the best action sequence.
+        """Plans the best action sequence.
 
         Given the current observation, sample many action sequence. Compute the
         cumulative rewards, choose the best action sequence with the highest reward.
 
         Args:
-            obs: The current observation
+            obs: The current observation.
 
         Returns:
             act_sequence (torch.Tensor): The best action sequence.
             state_sequence (torch.Tensor): The state sequence in the best rollout.
             total_reward (torch.Tensor): The total reward of the best rollout.
         """
-        device = obs.device
         action_sequences = self.action_sampler(obs, self.planning_steps)
+        action_sequences = action_sequences.to(self.device)
+
         num_rollouts = action_sequences.shape[0]
-        total_rewards = torch.zeros((num_rollouts,))
+        total_rewards = torch.zeros((num_rollouts,), device=self.device)
         with torch.no_grad():
             initial_state = self.generative_env.dynamics_model.state_from_observation(
                 obs
             )
+            initial_state = torch.tensor(initial_state, device=self.device)
             state_rollouts = torch.empty(
                 [num_rollouts, self.planning_steps + 1] + list(initial_state.shape),
-                device=device,
+                device=self.device,
             )
             # Prepend one dimension, repeat initial_state along this dimension for
             # num_rollouts times.
@@ -71,10 +82,18 @@ class RandomShootingAgent(GymAgent):
                 ) = self.generative_env.step(
                     state_rollouts[:, i, ...], action_sequences[:, i, :]
                 )
-                total_rewards += self.discount_factor**i * rewards
+                total_rewards += self.discount_factor ** i * rewards
             best_reward, best_rollout_index = total_rewards.max(dim=0)
 
             act_sequence = action_sequences[best_rollout_index]
             state_sequence = state_rollouts[best_rollout_index]
 
-            return act_sequence, state_sequence, best_reward
+            info = dict(
+                action_sequence=act_sequence,
+                best_reward=best_reward,
+            )
+            if self.verbose_info:
+                info["state_sequence"] = state_sequence
+
+            action = act_sequence[0].detach().cpu().numpy()
+            return action, info
---
+++ mani_skill2/dough_model_learning/dough_reward_models.py
@@ -0,0 +1,19 @@
+from typing import Tuple, Dict
+
+import torch
+
+from mani_skill2.dynamics.reward import GoalBasedRewardModel
+
+# TODO(blake.wulfe): Where to put this?
+class FlatDoughRollingRewardModel(GoalBasedRewardModel):
+    def step(
+        self, state: torch.Tensor, obs: torch.Tensor, action: torch.Tensor
+    ) -> Tuple[float, Dict]:
+        dims = tuple(range(1, state.ndim))
+        num_pos = (state > 0).sum(dim=dims)
+        avg_pos = state.sum(dim=dims) / num_pos
+        rew = -avg_pos * 10
+        return rew, {}
+
+    def set_goal(self, goal):
+        pass
